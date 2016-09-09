0060dc76ec32740c9acea487ebd46707d725b97e
Aquakor <kornelux1@gmail.com>
2016-09-09 23:02:08 +0200

    App before database.

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.shoppinglist;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,41 +22,48 @@ public class MainActivity extends AppCompatActivity {
     /** ArrayList for Placeholder name of products */
     private ArrayList<Product> mProductList;
 
+    private AutoCompleteTextView actv;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        String[] products = getResources().getStringArray(R.array.products);
+        actv = (AutoCompleteTextView) findViewById(R.id.product_edit_text);
+        ArrayAdapter<String> editTextAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,products);
+        actv.setAdapter(editTextAdapter);
+
         ListView listView = (ListView) findViewById(R.id.product_list_view);
 
         // Make new list for products
-        mProductList = new ArrayList<Product>();
-
-        mProductList.add(new Product("Mleko"));
-        mProductList.add(new Product("Masło"));
-
-        mAdapter = new SingleListAdapter(this, mProductList);
+        mAdapter = new SingleListAdapter(this, new ArrayList<Product>());
 
         listView.setAdapter(mAdapter);
 
+        /**
+         * When add button is pushed it adds product name
+         * from AutoCompleteTextView into mAdapter.
+         * When the AutoCompleteTextView is empty it
+         * returns Toast message...
+         */
         Button add = (Button) findViewById(R.id.add_button);
-
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //EditText temp = (EditText) findViewById(R.id.product_edit_text);
-                //mAdapter.add(new Product(temp.getText().toString()));
-                //temp.setText("");
-                mProductList.remove(0);
-                mAdapter.notifyDataSetChanged();
+                if(actv.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Enter product name!", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAdapter.add(new Product(actv.getText().toString()));
+                    actv.setText("");
+                }
+
             }
         });
 
-    }
-
-    public Void removeProduct(int position){
-        mProductList.remove(position);
-        mAdapter.notifyDataSetChanged();
-        return null;
     }
 }
